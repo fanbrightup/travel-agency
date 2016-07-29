e2468b4879c04c70fff3a3cd50d802e9b71474cf
fanbrightup <fanbright@126.com>
2016-07-29 14:45:35 +0800

    保存,稍作休息,真的这是一个令人兴奋的活动,或工作

--- mytravel.js
+++ mytravel.js
@@ -7,16 +7,32 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
+//  虚拟幸运饼干
+var fortunes = [
+
+  "1 + 1 = 2",
+  "2 + 2 = 4",
+  "3 + 3 = 6",
+  "4 + 4 = 8",
+  "5 + 5 = 10",
+];
+
+
 
 
 //  设置端口
 app.set('port',process.env.PORT || 3000);
+//  设置static中间件
+app.use(express.static(__dirname+'/public'));
+
 //  设置路由
 app.get('/',function(req,res){
   res.render('home');
 });
 app.get('/about',function(req,res){
-  res.render('about');
+  var randomFortune =
+      fortunes[Math.floor(Math.random()*fortunes.length)];
+  res.render('about',{fortune:randomFortune});
 })
 //  定制404页面
 app.use(function(req,res,next){
