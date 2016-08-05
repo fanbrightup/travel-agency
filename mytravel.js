accb1e384632e9c83dccf935beaa2ae4975e40a4
fanbrightup <fanbright@126.com>
2016-08-05 23:51:06 +0800

    增加的map路由界面,实现了通过城市名定位地图的简单功能

--- mytravel.js
+++ mytravel.js
@@ -1,24 +1,11 @@
 var express = require('express');
 var app = express();
-
+var fortune = require('./lib/fortune.js');
 //  设置handlebars视图引擎
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
-
-//  虚拟幸运饼干
-var fortunes = [
-
-  "1 + 1 = 2",
-  "2 + 2 = 4",
-  "3 + 3 = 6",
-  "4 + 4 = 8",
-  "5 + 5 = 10",
-];
-
-
-
 
 //  设置端口
 app.set('port',process.env.PORT || 3000);
@@ -30,9 +17,11 @@ app.get('/',function(req,res){
   res.render('home');
 });
 app.get('/about',function(req,res){
-  var randomFortune =
-      fortunes[Math.floor(Math.random()*fortunes.length)];
-  res.render('about',{fortune:randomFortune});
+  res.render('about',{fortune:fortune.getFortune()});
+})
+
+app.get('/map',function(req,res){
+  res.render('map');
 })
 //  定制404页面
 app.use(function(req,res,next){
@@ -45,7 +34,7 @@ app.use(function(err,req,res,next){
   res.status(500);
   res.render('500');
 });
-
+//  监听端口
 app.listen(app.get('port'),function(){
   console.log('Express started on http://localhost:'+
     app.get('port')+';press Ctrl-C to terminate.');
